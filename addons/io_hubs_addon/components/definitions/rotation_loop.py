5d15ac9dd0519aa8da7a674b8f1b362417bd0a5d
Hugo Sarti <sarhugo@gmail.com>
2022-10-10 03:54:26 -0300

    Rotation animation loop

---
+++ addons/io_hubs_addon/components/definitions/rotation_loop.py
@@ -0,0 +1,26 @@
+from bpy.props import FloatProperty, EnumProperty
+from ..hubs_component import HubsComponent
+from ..types import Category, PanelType, NodeType
+
+class Rotation(HubsComponent):
+    _definition = {
+        'name': 'rotation-loop',
+        'display_name': 'Rotation',
+        'category': Category.ANIMATION,
+        'node_type': NodeType.NODE,
+        'panel_type': [PanelType.OBJECT, PanelType.BONE],
+        'icon': 'FORCE_MAGNETIC'
+    }
+
+    speed: FloatProperty(
+        name="Speed",
+        description="In seconds",
+        default=1.0
+    )
+
+    axis: EnumProperty(
+        name="Axis",
+        description="Rotate on axis",
+        items=[("x", "X", "X Axis"), ("y", "Y", "Y Axis"), ("z", "Z", "Z Axis")],
+        default="z")
+    
