c32a0a9dadb5099d677147db7efacd83f4451b31
Hugo Sarti <sarhugo@gmail.com>
2022-10-19 23:32:50 -0300

    Room link component

---
+++ addons/io_hubs_addon/components/definitions/room_link.py
@@ -0,0 +1,20 @@
+from bpy.props import StringProperty
+from ..hubs_component import HubsComponent
+from ..types import Category, PanelType, NodeType
+
+
+class RoomLink(HubsComponent):
+    _definition = {
+        'name': 'room-link',
+        'display_name': 'Room Link',
+        'category': Category.ELEMENTS,
+        'node_type': NodeType.NODE,
+        'panel_type': [PanelType.OBJECT, PanelType.BONE],
+        'icon': 'LINKED'
+    }
+
+    src: StringProperty(name="Room URL", description="Room absolute url",
+                         default="")
+
+    text: StringProperty(name="Label", description="Text to display on hover",
+                         default="Exit")
